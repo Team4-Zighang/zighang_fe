af1944f3bde259a4016e42984752656ce5bb2b13
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-08-31 18:41:41 +0900

    #14 [CHORE] 주석추가 및 배너 스타일링 적용

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -13,25 +13,36 @@ const page = () => {
   return (
     <>
       <Header />
+      {/* 상단 배너 */}
       <div className="hidden flex-col md:block">
-        <div className="flex h-[320px] w-full flex-col bg-[url('/images/zighang_bookmark.png')] bg-cover bg-center pt-[80px] pb-[32px]">
-          <span className="text-contents-primary-accent">북마크</span>
-          <span className="">내가 담은 기회, 한눈에 확인하세요!</span>
-          <span className="body-xl-regular text-[#474748]">
-            저장해둔 공고와 메모를 언제든 간편하게 볼 수 있어요
-          </span>
-          <span className="text-contents-state-unselected">
-            * 내가 북마크한 기업과 직무는 동문에게 공유될 수 있어요. 단, 서류와
-            메모 등의 개인 정보는 공유되지 않으니 안심하세요.
-          </span>
+        <div className="flex h-[320px] w-full bg-[url('/images/zighang_bookmark.png')] bg-cover bg-center pt-[80px] pb-[32px]">
+          <div className="mx-auto flex h-full w-[1200px] flex-col justify-between">
+            <div className="flex flex-col">
+              <span className="text-contents-primary-accent body-xl-semibold">
+                북마크
+              </span>
+              <span className="heading-3xl-semibold py-[8px]">
+                내가 담은 기회, 한눈에 확인하세요!
+              </span>
+              <span className="body-xl-regular text-[#474748]">
+                저장해둔 공고와 메모를 언제든 간편하게 볼 수 있어요
+              </span>
+            </div>
+            <span className="text-contents-state-unselected body-sm-medium">
+              * 내가 북마크한 기업과 직무는 동문에게 공유될 수 있어요. 단,
+              서류와 메모 등의 개인 정보는 공유되지 않으니 안심하세요.
+            </span>
+          </div>
         </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="flex w-[1200px] flex-col gap-[32px] py-[64px]">
+          {/* 검색창 */}
           <SearchBar
             mdWidth="w-full"
             placeholder="직무 혹은 기업을 검색해보세요"
           />
+          {/* 필터 */}
           <div className="flex flex-col gap-[16px]">
             <div className="flex justify-between">
               <div className="flex items-center">
@@ -60,6 +71,7 @@ const page = () => {
             <BookMarkFilter />
           </div>
         </div>
+        {/* 북마크 리스트 */}
         <div className="py-[48px]">공고분석</div>
       </div>
       <div className="block md:hidden">mobile</div>
