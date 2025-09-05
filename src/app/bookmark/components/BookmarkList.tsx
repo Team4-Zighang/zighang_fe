506584144fccc35d4714e55068bb8881d9b39c48
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-09-06 03:01:53 +0900

    #14 [FEAT] 북마크 존재하지 않을 때 UI

--- src/app/bookmark/components/BookmarkList.tsx
+++ src/app/bookmark/components/BookmarkList.tsx
@@ -5,9 +5,10 @@ import { useMemo, useState } from 'react';
 
 // 더미데이터, 추후 API 연동 필요
 const data: BookmarkListItemProps[] = [
+  /*
   {
     id: 1,
-    dday: 3,
+    dday: '마감',
     title: 'Product Management',
     company: '(주)삼성전자',
     requirement: '• B2B 서비스 기획/PM/PMO 관련 업무 경험이 있는 분',
@@ -20,7 +21,7 @@ const data: BookmarkListItemProps[] = [
   },
   {
     id: 2,
-    dday: 3,
+    dday: '상시',
     title: 'Product Designer',
     company: '당근마켓',
     requirement: '• 5년 이상의 모바일, 웹 서비스 디자인 경험이 있으신 분',
@@ -42,6 +43,19 @@ const data: BookmarkListItemProps[] = [
     selected: false,
     expanded: false,
   },
+  {
+    id: 4,
+    dday: 10,
+    title: 'NCP 프로덕트 디자인',
+    company: '네이버클라우드',
+    requirement: '• 클라우드 또는 대규모 포털 서비스 디자인 경험이 있으신 분',
+    preference: '• 클라우드 또는 대규모 포털 서비스 디자인 경험이 있으신 분',
+    memo: '3급 채용이라 좀 빡셀 거 같은... 그래도 우선 지원은 했다. 저번 상반기에는 그래도 서류까지는 붙었으니까 이번에도 가능성 좀 있지',
+    docs: false,
+    selected: false,
+    expanded: false,
+  },
+   */
 ];
 
 const BookmarkList = () => {
@@ -74,7 +88,27 @@ const BookmarkList = () => {
     );
   };
 
-  return (
+  return items.length === 0 ? (
+    <div className="flex flex-col items-center gap-[12px] py-[64px]">
+      <Image
+        src="/icons/bookmark.svg"
+        alt="empty bookmark"
+        width={40}
+        height={40}
+      />
+      <div className="flex flex-col items-center gap-[4px]">
+        <span className="body-2xl-semibold text-contents-neutral-primary">
+          담겨있는 북마크가 없어요 😢
+        </span>
+        <span className="body-md-medium text-contents-neutral-tertiary">
+          마음에 드는 공고를 보고 북마크에 담아보세요!
+        </span>
+      </div>
+      <div className="bg-base-primary-default text-contents-state-inverse rounded-[12px] px-[24px] py-[12px]">
+        공고 보러가기
+      </div>
+    </div>
+  ) : (
     <div className="flex flex-col gap-[16px]">
       <div className="flex justify-between">
         <div className="flex items-center gap-[8px]">
