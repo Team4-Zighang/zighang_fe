99d9975272a4eadf012600925738447c5ef71109
chasyuss <cotn963@gmail.com>
2025-09-11 02:07:10 +0900

    #20 [CHORE] axios/query 설치

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['cdn.rallit.com', 'd2juy7qzamcf56.cloudfront.net'],
+  },
 };
 
 export default nextConfig;
