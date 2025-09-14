0fd4634d9fb84184a09cb96294fbb8b1e17ef65a
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-09-15 00:33:21 +0900

    #22 [CHORE] 직행 분석 변경사항 적용

--- src/app/bookmark/components/AnalyzeCard.tsx
+++ src/app/bookmark/components/AnalyzeCard.tsx
@@ -1,45 +1,10 @@
-import { GetPersonalityAnalysis } from '@/app/_apis/bookmark';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import AnalyzeBar from './AnalyzeBar';
-import { PersonalityResponse } from '@/app/_apis/schemas/bookmarkResponse';
 import { usePersonalityAnalysis } from '@/hooks/queries/useBookmark';
-
-const CHARACTER_MAP: Record<string, { img: string; desc: string }> = {
-  듬직행: {
-    img: '/images/characters/zighang_character_1.png',
-    desc: '대기업 정규직에서 안정적인 연봉과 복지를 중시하며, 출근이든 원격이든 든든하게 일할 수 있는 환경을 선호하시네요. 신뢰감이 느껴지는 듬직행이에요!',
-  },
-  실속행: {
-    img: '/images/characters/zighang_character_2.png',
-    desc: '대기업이든 스타트업이든, 연봉과 복지를 꼼꼼히 따지며 계약직의 실속을 챙기시네요. 현실 감각이 돋보이는 실속행이에요!',
-  },
-  성실행: {
-    img: '/images/characters/zighang_character_3.png',
-    desc: '안정적인 정규직을 바탕으로, 출근과 원격 근무를 통해 다양한 경험과 성장을 추구하시네요. 차근차근 발전해 나가는 성실행이에요!',
-  },
-  단짠행: {
-    img: '/images/characters/zighang_character_4.png',
-    desc: '대기업에 계약직으로 합류해, 안정 속에서도 새로운 경험과 배움을 찾아가시네요. 달콤쌉쌀한 도전을 즐기는 단짠행이에요!',
-  },
-  모험행: {
-    img: '/images/characters/zighang_character_5.png',
-    desc: '대기업의 안정된 환경에서 원격으로 일하며, 연봉과 복지보다 다양한 경험을 우선시하시네요. 정규직보다는 새로운 도전을 즐기는 모험행이에요!',
-  },
-  찐동행: {
-    img: '/images/characters/zighang_character_6.png',
-    desc: '스타트업에서 정규직으로 출근하며, 연봉과 복지를 챙기면서도 함께 성장하는 길을 선택하셨네요. 진득하게 동행하는 찐동행이에요!',
-  },
-  도전행: {
-    img: '/images/characters/zighang_character_7.png',
-    desc: '스타트업에서 출근하며 계약직으로 다양한 경험과 배움을 쌓는 길을 선택하셨네요. 새로운 기회를 향해 과감히 나아가는 도전행이에요!',
-  },
-  자유행: {
-    img: '/images/characters/zighang_character_8.png',
-    desc: '스타트업의 유연한 원격 근무를 통해 연봉·복지와 배움을 균형 있게 선택해가시네요. 자유로운 발걸음이 매력적인 자유행이에요!',
-  },
-};
+import Loader from '@/app/_components/common/Loader';
+import { CHARACTER_MAP } from '@/utils/character';
 
 const AnalyzeCard = () => {
   const {
@@ -55,17 +20,48 @@ const AnalyzeCard = () => {
       ? CHARACTER_MAP[personality.data.characterName]
       : CHARACTER_MAP['듬직행'];
 
-  if (isLoadingPersonality || isFetching) return <div>로딩중...</div>;
-  if (isError) return <div>분석중입니다...</div>;
+  if (isLoadingPersonality || isFetching)
+    return (
+      <div className="flex h-[360px] w-full items-center justify-center">
+        <Loader size={20} />
+      </div>
+    );
 
   return (
     <div className="md:px-auto flex w-full flex-col gap-[16px] px-[20px] py-[48px] md:w-[1200px]">
       <span className="heading-sm-semibold md:heading-1xl-semibold">
         지우님이 북마크한 공고들을 분석했어요!
       </span>
-      <div className="bg-base-primary-alternative flex w-full flex-col gap-[24px] rounded-[16px] p-[12px] md:h-[360px] md:flex-row md:gap-[0px] md:p-[0px]">
+      <div
+        className={`bg-base-primary-alternative flex w-full flex-col gap-[24px] rounded-[16px] p-[12px] md:h-[360px] md:flex-row md:gap-[0px] md:p-[0px] ${isError ? 'md:justify-between' : ''}`}
+      >
         {isError ? (
-          <div className="">분석중입니다...</div>
+          <>
+            <div className="flex flex-col justify-between gap-[8px] p-[8px] md:gap-[80px] md:p-[40px]">
+              <div className="flex flex-col">
+                <span className="text-contents-primary-accent body-lg-semibold md:body-xl-semibold">
+                  북마크 수가 부족해요
+                </span>
+                <span className="text-contents-neutral-primary heading-2xl-semibold md:title-sm-semibold">
+                  공고 3개 북마크하면
+                  <br />
+                  나의 취업유형을 찾아줘요
+                </span>
+              </div>
+              <span className="body-md-medium md:body-lg-regular text-contents-neutral-tertiary">
+                공고를 3개 이상 북마크하면 취업 유형을 찾아드릴게요.
+                <br />
+                취업유형을 바탕으로 나와 맞는 공고를 추천받을 수 있어요
+              </span>
+            </div>
+            <Image
+              className="w-full object-contain md:mr-[32px] md:w-[360px]"
+              src="/images/characters/zighang_character_1.png"
+              alt="zighang character"
+              width={360}
+              height={360}
+            />
+          </>
         ) : (
           <>
             <Image
