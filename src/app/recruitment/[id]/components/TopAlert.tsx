fbd79fb0b35ecf42e13a12e834bdcbd372acdd27
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-09-16 15:32:02 +0900

    #30 [CHORE] 북마크/메모 알림 사라지기

--- src/app/recruitment/[id]/components/TopAlert.tsx
+++ src/app/recruitment/[id]/components/TopAlert.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 type TopAlertProps = {
@@ -5,29 +6,49 @@ type TopAlertProps = {
   onClose: () => void;
 };
 
-const TopAlert = ({ message, onClose }: TopAlertProps) => (
-  <div className="fixed top-15 -right-50 z-9999 flex w-[320px] -translate-x-1/2 flex-col items-center">
-    <Image
-      src="/icons/polygon_2.svg"
-      alt=""
-      width={18}
-      height={9}
-      className=""
-    />
-    <div className="bg-contents-neutral-primary flex gap-[8px] rounded-[8px] py-[12px] pr-[12px] pl-[16px]">
-      <div className="text-contents-state-inverse body-md-semibold">
-        {message}가 추가됐어요!
+const TopAlert = ({ message, onClose }: TopAlertProps) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setVisible(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!visible) {
+      const timer = setTimeout(onClose, 500);
+      return () => clearTimeout(timer);
+    }
+  }, [visible, onClose]);
+
+  return (
+    <div
+      className={`fixed top-15 -right-25 z-9999 flex w-[320px] -translate-x-1/2 flex-col items-center transition-opacity duration-500 ${
+        visible ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      <Image
+        src="/icons/polygon_2.svg"
+        alt=""
+        width={18}
+        height={9}
+        className=""
+      />
+      <div className="bg-contents-neutral-primary flex gap-[8px] rounded-[8px] py-[12px] pr-[12px] pl-[16px]">
+        <div className="text-contents-state-inverse body-md-semibold">
+          {message}가 추가됐어요!
+        </div>
+        <button onClick={onClose}>
+          <Image
+            src="/icons/x_button_white.svg"
+            alt="close"
+            width={24}
+            height={24}
+          />
+        </button>
       </div>
-      <button onClick={onClose}>
-        <Image
-          src="/icons/x_button_white.svg"
-          alt="close"
-          width={24}
-          height={24}
-        />
-      </button>
     </div>
-  </div>
-);
+  );
+};
 
 export default TopAlert;
