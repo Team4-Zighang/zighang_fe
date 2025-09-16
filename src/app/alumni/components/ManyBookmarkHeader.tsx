01d6dc20561495d14b0e0f0e445a36f983befced
chasyuss <cotn963@gmail.com>
2025-09-17 03:01:20 +0900

    #36 [FIX] 비로그인시 텍스트명 수정

--- src/app/alumni/components/ManyBookmarkHeader.tsx
+++ src/app/alumni/components/ManyBookmarkHeader.tsx
@@ -1,16 +1,24 @@
 'use client';
-import { getMember } from '@/store/member';
+import { getMember, getToken } from '@/store/member';
 import React from 'react';
 
 const ManyBookmarkHeader = () => {
   const memberData = getMember();
+  const token = getToken();
+
   return (
     <div className="text-contents-neutral-primary web-title2 md:heading-1xl-semibold">
-      {memberData?.member?.memberName}님의 동문들이
-      <span className="block md:inline">
-        {' '}
-        가장 많이 북마크한 공고를 살펴보세요!
-      </span>
+      {token && memberData?.member?.memberName ? (
+        <>
+          {memberData.member.memberName}님의 동문들이
+          <span className="block md:inline">
+            {' '}
+            가장 많이 북마크한 공고를 살펴보세요!
+          </span>
+        </>
+      ) : (
+        <>동문들이 가장 많이 북마크한 공고를 살펴보세요!</>
+      )}
     </div>
   );
 };
