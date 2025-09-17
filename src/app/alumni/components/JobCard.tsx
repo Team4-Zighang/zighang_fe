616750453a4c81e62e13166c33bd904ffd5f49e9
chasyuss <cotn963@gmail.com>
2025-09-17 20:13:08 +0900

    #41 [CHORE] 로더 크기 설정

--- src/app/alumni/components/JobCard.tsx
+++ src/app/alumni/components/JobCard.tsx
@@ -62,7 +62,7 @@ const JobCard = () => {
   if (isLoading)
     return (
       <div className="flex w-full items-center justify-center">
-        <Loader />
+        <Loader size={20} />
       </div>
     );
   if (isError) return <div className="text-center">에러가 발생했습니다.</div>;
