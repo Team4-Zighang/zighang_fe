de079b4ced0019c7bfa09fd4a63a5e0679bfaa96
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-09-18 14:01:14 +0900

    #43 [CHORE] 공고분석 비로그인 시 문구 변경

--- src/app/bookmark/components/AnalyzeCard.tsx
+++ src/app/bookmark/components/AnalyzeCard.tsx
@@ -40,7 +40,9 @@ const AnalyzeCard = () => {
   return (
     <div className="md:px-auto flex w-full flex-col gap-[16px] px-[20px] py-[48px] md:w-[1200px]">
       <span className="heading-sm-semibold md:heading-1xl-semibold">
-        {name ?? '??'}님이 북마크한 공고들을 분석했어요!
+        {name
+          ? `${name}님이 북마크한 공고들을 분석했어요!`
+          : '로그인하고 취업 유형을 분석해보세요!'}
       </span>
       <div
         className={`bg-base-primary-alternative flex w-full flex-col gap-[24px] rounded-[16px] p-[12px] md:h-[360px] md:flex-row md:gap-[0px] md:p-[0px] ${isError ? 'md:justify-between' : ''}`}
@@ -80,6 +82,7 @@ const AnalyzeCard = () => {
               alt="zighang character"
               width={360}
               height={360}
+              quality={100}
             />
             <div className="flex flex-1 flex-col gap-[8px] p-[8px] md:gap-[80px] md:p-0 md:py-[52px]">
               <div className="flex flex-col">
